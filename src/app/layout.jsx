91629517523e14d283ec9fa57ae56e6428db348c
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-05-10 14:39:02 +0400

    header

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+import styles from "./index.module.css"
 export const metadata = {
   title: "Esas title",
   description: "Bu esas proyektir",
@@ -7,9 +9,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <div>bu navbardir</div>
+        <div className={styles.box}>
+          <Link href="/" passHref>Home</Link>
+          <Link href="/about" passHref>About</Link>
+          <Link href="/contact" passHref>Contact</Link>
+          <Link href="/users" passHref>Users</Link>
+        </div>
         {children}
-        <div>bu footerdir</div>
+        <div></div>
       </body>
     </html>
   );
