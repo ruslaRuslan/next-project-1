12efc2191fe4ff4325691a5432650e9c563e86ee
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-05-20 21:13:42 +0400

    user-in responsivliyi hazir

--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -3,7 +3,7 @@ import * as React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import { CardActionArea, Grid } from "@mui/material";
+import { Box, CardActionArea, Grid } from "@mui/material";
 import Link from "next/link";
 
 const url = "https://jsonplaceholder.typicode.com/users";
@@ -15,31 +15,36 @@ export default function index() {
     <>
       {data.map(({ id, name }) => {
         return (
-          <Grid>
-            <Card
-              sx={{
-                maxWidth: 345,
-                border: "1px solid #000",
-              }}
-            >
-              <CardActionArea>
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    <Link
-                      href={`/users/${id}`}
-                      style={{ textDecoration: "none", color: "#000" }}
-                    >
-                      {id}. {name}
-                    </Link>
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="text.secondary"
-                  ></Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
+          <Box>
+            <Grid>
+              <Card
+                sx={{
+                  maxWidth: 345,
+                  border: "1px solid #000",
+
+                  display: "blok", // почему не работают?  я хочу чтобы они все были на середине по вертикали на
+                  margin: "0 auto", // почему не работают?  я хочу чтобы они все были на середине по вертикали на одной линии
+                }}
+              >
+                <CardActionArea>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      <Link
+                        href={`/users/${id}`}
+                        style={{ textDecoration: "none", color: "#000" }}
+                      >
+                        {id}. {name}
+                      </Link>
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="text.secondary"
+                    ></Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          </Box>
         );
       })}
     </>
